7e2156359266c878ee88782529c33162e484326c
Blueness <w0330t@gmail.com>
2017-08-26 12:24:31 +0800

    Add a click event

--- app/src/main/java/net/blueness/recyclerviewtest/FruitAdapter.kt
+++ app/src/main/java/net/blueness/recyclerviewtest/FruitAdapter.kt
@@ -1,11 +1,13 @@
 package net.blueness.recyclerviewtest
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import net.blueness.listviewtest.Fruit
 import org.jetbrains.anko.find
 
@@ -17,13 +19,30 @@ class FruitAdapter(private val mFruitList: MutableList<Fruit>):
         RecyclerView.Adapter<FruitAdapter.ViewHolder>() {
 
     class ViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val fruitView = view
         val fruitImage: ImageView = view.find(R.id.fruit_image)
         val fruitName: TextView = view.find(R.id.fruit_name)
     }
 
+
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder{
         val view = LayoutInflater.from(parent!!.context).inflate(R.layout.fruit_item, parent, false)
-        return ViewHolder(view)
+        val holder = ViewHolder(view)
+        holder.fruitView.setOnClickListener{
+            v: View ->
+            //这两行应该是点击后的事件，必须放这里面
+            val position = holder.adapterPosition
+            val fruit = mFruitList.get(position)
+            Toast.makeText(v.context, "You clicked view" + fruit.name, Toast.LENGTH_SHORT).show()
+        }
+        holder.fruitImage.setOnClickListener{
+            v: View ->
+            val position = holder.adapterPosition
+            val fruit = mFruitList.get(position)
+            Toast.makeText(v.context, "You clicked image" + fruit.name, Toast.LENGTH_SHORT).show()
+            Unit
+        }
+        return holder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
